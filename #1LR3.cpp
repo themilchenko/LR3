4842f8af65a4048203e7ca670dd3e25e02a7b789
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-09-23 22:50:54 +0300

    Add files via upload

---
+++ #1LR3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+int main()
+{
+	std::srand(std::time(0));
+	int a[10];
+	std::cout << "Array: ";
+	for (int i = 0; i < 10; i++)
+	{
+		a[i] = std::rand() % 21 - 10;
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl << "Resulting sum: ";
+	int sum = pow(a[0],2) + pow(a[1],2) + pow(a[5],2);
+	std::cout << pow(a[0],2) << " + " << pow(a[1],2) << " + " << pow(a[5],2) << " = " << sum;
+	return 0;
+}
