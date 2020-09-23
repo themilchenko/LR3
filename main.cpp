d189d0f312de2d4f5fc6a5dc6a2a9761a9e12b8e
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-09-23 15:54:09 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
-#include <ctime>
 #include <cmath>
-int main()
-{
-    std::srand(std::time(0));
-    int arr1[10];
-    std::cout << "Array: ";
-    for (int i = 0; i < 10; i++) {
-        arr1[i] = std::rand() % 21 - 10;
-        std::cout << arr1[i] << " ";
-    }
-    std::cout << std::endl << "Resulting sum: ";
-    int sum = pow(arr1[0],2) + pow(arr1[1],2) + pow(arr1[5],2);
-    std::cout << pow(arr1[0],2) << " + " << pow(arr1[1],2) << " + " << pow(arr1[6],2) << " = " << sum;
+#include <vector>
+int main() {
+    int n;
+    int i;
+    int *arr = new int [n];
+    std::cout << "Enter the number (n > 10) of values: ";
+    std::cin >> n;
+    int a[n];
+    std::cout<< std::endl << "Enter the numbers of array: ";
+    for (i = 0; i < n; i++)
+        std::cin >> a[i];
+    std::cout << std::endl << "Array: ";
+    for (i = 0; i < n; i++)
+        std::cout << a[i] << " ";
+    for (i = 0; i < n; i++)
+        if ((a[i] % 2) == 0)
+            a[i] /= 3;
+    std::cout << std::endl << "Array (/3): ";
+    for (i = 0; i < n; i++)
+        std::cout << a[i] << " ";
+    std::cout << std::endl << "Enter 2 numbers of array to count average value: ";
+    int k1, k2;
+    std::cout << std::endl << "enter the k1: ";
+    std::cin >> k1;
+    std::cout << std::endl << "enter the k2: ";
+    std::cin >> k2;
+    int sum = 0;
+    for(i = k1; i < k2; i++)
+        sum += a[i];
+    double average = sum / abs(k1 - k2);
+    std::cout << "Average value: " << average;
     return 0;
 }
