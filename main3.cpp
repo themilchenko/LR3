1bffaaa71dabde63a2d61eb9187c1d7fa8fa217e
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-11-19 20:24:16 +0300

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-int main() {
-    int m, i, sum = 0;
+
+int main() 
+{
+    int m, sum = 0;
     std::cout << "Enter the number of array (>5): ";
     std::cin >> m;
     std::vector <int> a(m);
     std::cout << "Enter numbers of array: ";
-    for (i = 0; i < m; i++)
+    for (int i = 0; i < a.size(); i++)
         std::cin >> a[i];
     std::cout << "Array: ";
-    for (i = 0; i < m; i++)
-        std::cout << a[i] << " ";
+    for (int i : a)
+        std::cout << i << " ";
     std::cout << std::endl;
     std::cout << "Array with my variant: ";
     a.push_back(16);
-    for (i = 0; i < m + 1; i++)
-        std::cout << a[i] << " ";
-    for (i = 0; i < m + 1; i++)
+    for (int i : a)
+        std::cout << i << " ";
+    for (int i = 0; i < a.size(); i++)
         sum += pow(a[i], 2);
     std::cout << std::endl << "Sum: " << sum;
     std::cout << std::endl << "Final array: ";
-    for (i = 0; i < m +1; i++)
+    for (int i = 0; i < a.size(); i++)
     {
         if (a[i] > 3)
         {
-            a.erase(a.begin()+i);
+            a.erase(a.begin() + i);
             break;
         }
     }
-    for (i = 0; i < m; i++)
-        std::cout << a[i] << " ";
+    for (int i : a)
+        std::cout << i << " ";
     return 0;
 }
